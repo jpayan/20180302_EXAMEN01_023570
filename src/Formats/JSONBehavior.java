0278b1b8e0f58993e2347cbc2a3dacc2360112fb
Jorge Payan <jalexpayan@gmail.com>
2018-03-02 20:57:21 -0800

    Update Test

--- src/Formats/JSONBehavior.java
+++ src/Formats/JSONBehavior.java
@@ -5,7 +5,7 @@ import java.util.Map;
 public class JSONBehavior implements FormatBehavior {
     @Override
     public Object parse(Map<String, Object> message) {
-        System.out.println("Parsed message to JSON");
+        System.out.println("Message parsed to JSON");
         return message.toString();
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,22 @@
 import Banks.BanCetys;
+import Banks.BanPatito;
 import Banks.BanPayucas;
+import Formats.CSVBehavior;
 import Formats.JSONBehavior;
 import Formats.XMLBehavior;
+import Protocols.EmailBehavior;
 import Protocols.FTPBehavior;
 import Protocols.HTTPBehavior;
 
 public class Main {
     public static void main(String[] args) {
         BanCetys banCetys = new BanCetys("BanCetys", new HTTPBehavior(), new JSONBehavior(), "Calle Numero Ciudad");
-        BanPayucas banPayucas = new BanPayucas("BanPayucas", new HTTPBehavior(), new JSONBehavior(), "BC");
+        BanPayucas banPayucas = new BanPayucas("BanPayucas", new FTPBehavior(), new XMLBehavior(), "Head Quaters");
+        BanPatito banPatito = new BanPatito("BanPatito", new EmailBehavior(), new CSVBehavior(), "BC");
 
         banCetys.transfer(banCetys, banPayucas, 1000);
+        banPayucas.transfer(banPayucas, banPatito, 800);
+        banPatito.transfer(banPatito, banCetys, 100);
 
         banCetys.setFormatBehavior(new XMLBehavior());
         banCetys.setProtocolBehavior(new FTPBehavior());
